7486355f9b032c0508f13660642e560f74d2665e
ijusthahaha <ijusthahaha@outlook.com>
2024-06-04 09:21:07 +0800

    2024/6/4 bug fix

---
+++ src/main/java/website/hehe/interceptor/ExpiredJwtInterceptor.java
@@ -0,0 +1,26 @@
+package website.hehe.interceptor;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import website.hehe.utils.TokenSignKeyUtils;
+
+@Component
+@Setter(onMethod_ = @Autowired)
+public class ExpiredJwtInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String token = request.getHeader("token");
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(TokenSignKeyUtils.getTokenSignKey().getBytes())).build().parseSignedClaims(token);
+        } catch (Exception ignored) {
+            response.setStatus(401);
+            return false;
+        } return true;
+    }
+}
